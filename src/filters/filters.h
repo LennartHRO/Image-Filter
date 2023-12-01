2c048e747d2e492087fa5c43caefa67eb8540237
Alan Fritsche <go34ced@mytum.de>
2023-12-02 00:48:48 +0100

    Delete filters.h

--- src/filters/filters.h
+++
@@ -1,9 +0,0 @@
-#include <vector>
-
-void change_brightness(std::vector<std::vector<std::vector<int>>> &matrix, int brightness);
-
-void invert_image(std::vector<std::vector<std::vector<int>>> &matrix);
-
-void change_contrast(std::vector<std::vector<std::vector<int>>> &matrix, float contrast);
-
-void make_image_gray(std::vector<std::vector<std::vector<int>>> &matrix);
